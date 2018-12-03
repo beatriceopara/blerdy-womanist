90a6ea113ceeaa41e97913777c0de825d230306a
Beatrice <beatriceopara94@gmail.com>
2018-12-03 18:50:46 +0000

    sunday work
    added menu tab
    edited content

---
+++ parallax.js
@@ -0,0 +1,10 @@
+function openSM(){
+  document.getElementById("mySidemenu").style.width="300px";
+  document.getElementById("pg-content").style.marginleft="300px";
+  document.body.sttyle.backgroundcolour="black";
+}
+
+function closeSM(){
+  document.getElementById("mySidemenu").style.width="0";
+  document.getElementById("pg-content").style.marginleft="0";
+}
