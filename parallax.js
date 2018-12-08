999b2a8f1abac10f1161582802256e949a543a29
Beatrice <beatriceopara94@gmail.com>
2018-12-08 15:27:03 +0000

    topbtn
    add topbtn, with javascript

--- parallax.js
+++ parallax.js
@@ -8,3 +8,19 @@ function closeSM(){
   document.getElementById("mySidemenu").style.width="0";
   document.getElementById("pg-content").style.marginleft="0";
 }
+
+window.onscroll=function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop>20 ||
+  document.documentElement.scrollTop >20) {
+    document.getElementById("topbtn").style.display="block";
+  } else {
+    document.getElementById("topbtn").style.display="none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop= 0;
+  document.documentElement.scrollTop=0;
+}
